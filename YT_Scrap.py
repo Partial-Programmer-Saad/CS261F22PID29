619bfb812cc33502140552794352786393a1955a
Abdullahofficial00 <105612170+Abdullahofficial00@users.noreply.github.com>
2022-10-18 00:45:20 +0500

    CSV File

--- YT_Scrap.py
+++ YT_Scrap.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import csv
+import datetime
+from datetime import datetime
 option = webdriver.ChromeOptions()
-option.headless = False
+option.headless = True
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
 Links=[]
 driver.get('https://www.youtube.com/aashiqui2/videos')
@@ -13,11 +15,21 @@ driver.get('https://www.youtube.com/aashiqui2/videos')
 time.sleep(10)
 prev_h = 0
 while True:
-    height = driver.execute_script("""function getActualHeight(){return Math.max(Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),Math.max(document.body.offsetHeight, document.documentElement.offsetHeight),Math.max(document.body.clientHeight, document.documentElement.clientHeight));}return getActualHeight();""")
-    driver.execute_script("window.scrollTo({},{})".format(prev_h,prev_h+300))
+    height = driver.execute_script("""
+        function getActualHeight(){
+            return Math.max(
+                Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
+                Math.max(document.body.offsetHeight, document.documentElement.offsetHeight),
+                Math.max(document.body.clientHeight, document.documentElement.clientHeight)
+                );
+            }
+        return getActualHeight();
+    """)
+    driver.execute_script(
+        "window.scrollTo({},{})".format(prev_h,prev_h+300))
     time.sleep(2)
     prev_h += 300
-    if prev_h >= 10:
+    if prev_h >= height:
         break
 #------------------------------------------------------------------------------------------------------------------------
 content = driver.page_source
@@ -26,20 +38,30 @@ soup = BeautifulSoup(content,'lxml')
 #div_list = soup.findAll('div',attrs={'class':'style-scope ytd-grid-video-renderer'})
 Song_link = soup.findAll('a', attrs={'class':'yt-simple-endpoint style-scope ytd-grid-video-renderer'})
 
-time_duration=""
-print(Song_link.__len__())
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+Durations = soup.findAll('span' , attrs={'class':'style-scope ytd-thumbnail-overlay-time-status-renderer'})
+Song_t=[]
+for i in range(0,len(Durations)):
+    Song_t.append(Durations[i].text.strip().strip())
+    #print(Durations[i].text.strip().strip())
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+i=0
+#print(Song_link.__len__())
 for link in Song_link:
-    try:
-        time_duration = link.find('div', attrs={'class':'style-scope ytd-thumbnail'}).text
-        print(time_duration)
-        break
-    except AttributeError:
-        print("time duration error")
-        break
+    Song1=link.get('href')
+    Song1='https://www.youtube.com'+Song1
+    with open('Song_Links.csv','a',newline='', encoding='utf-8') as fd:
+        writer = csv.writer(fd)
+        writer.writerow([Song1])
+    fd.close()
+    #i+=1
+timecount=0
+for link in Song_link: 
     Song=link.get('href')
     Song='https://www.youtube.com'+Song
     driver.get(Song)
-    time.sleep(20)
+    time.sleep(15)
     content1 = driver.page_source
     soup1 = BeautifulSoup(content1,'lxml')
     div = soup1.find('div' , attrs={'class':'style-scope ytd-video-primary-info-renderer'})
@@ -48,52 +70,47 @@ for link in Song_link:
     views=""
     Upload_date=""
     likes=""
-    Song_time=""
     singer=""
+    itntity_count=0
     
-    
-    
+    #try:
+    #   Song_t = soup.find('div', attrs={'class':'ytp-time-display notranslate'}).text
+    #except AttributeError:
+    #    print("time duration error")
+    #    itntity_count+=1
     try:
         likes = div.find('a', attrs={'class':'yt-simple-endpoint style-scope ytd-toggle-button-renderer'}).text
     except AttributeError:
         print("likes error")
-        #break
+        itntity_count+=1
     
     try:
         title = div.find('h1', attrs={'class':'title style-scope ytd-video-primary-info-renderer'}).text
     except AttributeError:
         print("title error")
-        #break
+        itntity_count+=1
     try:
         views = div.find('span', attrs={'class':'view-count style-scope ytd-video-view-count-renderer'}).text
     except AttributeError:
         print("views error")
-        #break
+        itntity_count+=1
     try:
         Upload_date = div.find('div', attrs={'id':'info-strings'}).text
     except AttributeError:
         print("upload date error")
-        #break
+        itntity_count+=1
 
     count=0
     for i in title:
         if i=="|":
             count+=1
-        if count>=1 and count<2:
+        if count>=1 and count<3:
             if i!="|":
                 singer=singer+i
-    count1=0
-    for i in time_duration:
-        if i=="/":
-            count1+=1
-        if count1==1:
-            Song_time=Song_time+i
-
-
-    print(title , " " , singer , " " , views , " " , Upload_date , " " , likes , " " , Song_time, " " ,Song)
-    
-    with open('Song.csv','a',newline='', encoding='utf-8') as fd:
-        writer = csv.writer(fd)
-        writer.writerow([title ,singer , views , Upload_date ,likes , time_duration,Song])
-    fd.close()
-    break
+    #singer="Billie Eilish"
+    if itntity_count==0:
+        with open('Song.csv','a',newline='', encoding='utf-8') as fd:
+            writer = csv.writer(fd)
+            writer.writerow([title ,singer , views , Upload_date ,likes , Song_t[timecount],Song])
+        fd.close()
+    timecount+=1
